b053af320ea30e23e1f26894ed1b99f3a26f52e5
juan francisco gonzalez cerrato <jfgonzalezc02@gmail.com>
2023-09-27 16:14:38 +0200

    terminado

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const daysElement = document.getElementById('dias');
-
 const minsElement = document.getElementById('minutos');
 const secondsElement = document.getElementById('segundos');
+const countdownContainer = document.querySelector('.countdown-container');
 const upcomingImg = document.getElementById('upcoming-img');
+const h1Element = document.querySelector('h1');
 
 let currentDate = new Date();
-let goalDate = new Date(2023, 11, 26, 1, 1); // Año, mes (enero==0), día, hora, minutos¡
+let goalDate = new Date(2023, 10, 26, 1, 1); // Año, mes (enero==0), día, hora, minutos
+
 let days, mins, seconds, totalSeconds;
 
-//Crea un intervalo que llama a la función cada segundo
+// Crea un intervalo que llama a la función cada segundo
 let countdownInterval = setInterval(countdown, 1000);
 countdown();
 
@@ -18,29 +20,57 @@ function countdown() {
 
   // Condición para comprobar si ha llegado la hora establecida
   if (Math.floor(totalSeconds) <= 0) {
-    showProduct();
-    secondsElement.innerHTML = 0;
+    showBirthdayMessage();
+    clearInterval(countdownInterval);
     return;
   }
 
-  //Para saber el equivalente de 1 segundo - dias se dividen los segundos entre 86400 o entre 3600 y luego entre 24
-  //Para saber el equivalente de 1 segundo - horas se dividen los segundos entre 3600
-  //Para saber el equivalente de 1 segundo - minutos se dividen los segundos entre 60
-
   days = Math.floor(totalSeconds / 3600 / 24);
-
   mins = Math.floor(totalSeconds / 60) % 60;
   seconds = Math.floor(totalSeconds) % 60;
 
   daysElement.innerHTML = days;
-
   minsElement.innerHTML = mins;
   secondsElement.innerHTML = seconds;
-
-};
-
-function showProduct() {
-  upcomingImg.classList.remove('nocolor-img');
-  //Paramos el intervalo que se estaba ejecutando
-  clearInterval(countdownInterval);
 }
+
+function showBirthdayMessage() {
+	// Ocultar el contador y mostrar el mensaje de cumpleaños
+	countdownContainer.style.display = 'none';
+	h1Element.textContent = '¡Felicidades!';
+  
+	// Crear un elemento de imagen para la tarta
+	const tartaImg = document.createElement('img');
+	tartaImg.src = './imagenes/tarta.png'; // Ruta relativa de la imagen de la tarta
+	tartaImg.alt = 'Tarta de cumpleaños';
+  
+	// Agregar la imagen de la tarta al cuerpo del documento
+	document.body.appendChild(tartaImg);
+  }
+  
+  
+
+// Función para cambiar el fondo según la estación del año
+function changeBackgroundBySeason() {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1; // Suma 1 ya que los meses en JavaScript van de 0 a 11
+
+  let seasonClass = '';
+
+  // Determina la estación del año según el mes actual
+  if (currentMonth >= 3 && currentMonth <= 5) {
+    seasonClass = 'spring-background'; // Primavera: marzo (3) a mayo (5)
+  } else if (currentMonth >= 6 && currentMonth <= 8) {
+    seasonClass = 'summer-background'; // Verano: junio (6) a agosto (8)
+  } else if (currentMonth >= 9 && currentMonth <= 11) {
+    seasonClass = 'autumn-background'; // Otoño: septiembre (9) a noviembre (11)
+  } else {
+    seasonClass = 'winter-background'; // Invierno: diciembre (12), enero (1) y febrero (2)
+  }
+
+  // Aplica la clase correspondiente al cuerpo de la página
+  document.body.classList.add(seasonClass);
+}
+
+// Llama a la función para cambiar el fondo al cargar la página
+changeBackgroundBySeason();
